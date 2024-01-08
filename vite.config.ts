a23f279aea400824a80590f2ae923c7b78d6b09d
ferasatali <ferasatali14@gmail.com>
2024-01-08 11:59:24 +0500

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 // Plugins
-import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import ViteFonts from 'unplugin-fonts/vite'
+import vue from "@vitejs/plugin-vue";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import ViteFonts from "unplugin-fonts/vite";
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,28 +21,26 @@ export default defineConfig({
       google: {
         families: [
           {
-            name: 'Inter',
-            styles: 'wght@100;200;300;400;500;600;700;800;900',
+            name: "Inter",
+            styles: "wght@100;200;300;400;500;600;700;800;900",
           },
         ],
       },
     }),
   ],
-  base: './', 
-  define: { 'process.env': {} },
+  base: "/ferasat.github.io/",
+  // base:'./',
+  define: { "process.env": {} },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
   optimizeDeps: {
-    include: [
-      "@fawmi/vue-google-maps",
-      "fast-deep-equal",
-    ],
+    include: ["@fawmi/vue-google-maps", "fast-deep-equal"],
   },
   server: {
     port: 3000,
   },
-})
+});
