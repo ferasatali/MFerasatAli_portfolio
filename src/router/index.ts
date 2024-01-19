674fbed130caddfcab916cddc07f590549678039
ferasatali <ferasatali14@gmail.com>
2024-01-19 14:01:02 +0500

    Update router history and base path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/ferasatali.github.io/'),
+  history: createWebHashHistory(),
   routes,
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
       },
     }),
   ],
-  base: "/ferasatali.github.io/",
+  base: "/",
   // base:'./',
   define: { "process.env": {} },
   resolve: {
