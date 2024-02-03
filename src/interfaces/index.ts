bc20cfa5255fd3cba82f615ae055eea5da0dfe21
ferasatali <ferasatali14@gmail.com>
2024-02-03 12:43:07 +0500

    Update package.json and add social links interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,6 @@
 import * as IProjects from "@/interfaces/projects.interface";
 import * as IExperience from "@/interfaces/experience.interface";
 import * as IJourney from "@/interfaces/journey.interface";
+import * as ISocialLinks from "@/interfaces/socialLinks.interface";
 
-export { IProjects, IExperience, IJourney };
+export { IProjects, IExperience, IJourney, ISocialLinks };
