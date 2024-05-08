8252085edd07fa6844660b48bc6f1d552485e376
fali-aslase <fali@aslase.com>
2024-05-08 15:38:33 +0500

    chore: Update Google Spreadsheet API URL and optimize dependencies

--- src/utils/googleSpreadSheetAPI.ts
+++ src/utils/googleSpreadSheetAPI.ts
@@ -1,27 +1,34 @@
 import axios from "axios";
 
-// const Sheet =
-//   "https://docs.google.com/spreadsheets/d/16WUWHMMk0rgzh6Kc9kLIykYQwm9hRpUR07DLQXRWSE4/edit#gid=0";
+const dataToPost = {
+  name: "John Doe",
+  email: "example@gmail.com",
+  phone: "123-456-7890",
+  message: "Hello, this is a sample message.",
+} as any;
 
 export const loadSheetsApi = async () => {
-  const apiUrl = "https://script.google.com/macros/s/AKfycbxfmyi2wg2qhIN-FoIUnsX4gE8TjHDdrK_kzx5kcsbt-Ne0Gr21UhBSFXgw4SEPPXPv/exec"
-
-  const data = {
-    name: "John Doe",
-    email: "ferasatali14@gmail.com",
-    phone: "123-456-7890",
-    message: "Hello, this is a sample message.",
-  };
-  axios
-    .post(apiUrl, data, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-    .then((response) => {
-      console.log("Data posted successfully:", response);
-    })
-    .catch((error) => {
-      console.error("Error posting data:", error);
-    });
+  try {
+    const scriptURL =
+      "https://script.google.com/macros/s/AKfycbw4-QJWbzyvUmjWe0HVDpm2uU56UXIbdVSge7Uxpvxr3o4xN6QUrxQtXRmbvBJzSP0zaw/exec";
+    const formData = new FormData();
+    for (const key in dataToPost) {
+      formData.append(key, dataToPost[key]);
+    }
+    console.log(formData, "formData");
+    const response = await axios.post(scriptURL, formData,
+      {
+        headers: {
+          'Content-Type': 'text/plain',
+        }
+      });
+    console.log(response, "response");
+    if (response.status === 200) {
+      alert("Thank you! Your form is submitted successfully.");
+    } else {
+      throw new Error("Network response was not ok");
+    }
+  } catch (error: any) {
+    console.error("Error!", error.message);
+  }
 };
