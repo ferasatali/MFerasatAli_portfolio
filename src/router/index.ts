9fe4640f01c2fe1afe495f26776696dd3a8c66f8
fali-aslase <fali@aslase.com>
2024-05-08 15:43:09 +0500

    refactor: Update component import path in router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const routes = [
       {
         path: '/home',
         name: 'Home',
-        component: () => import('@/views/home.vue'),
+        component: () => import('@/views/Home.vue'),
       },
     ],
   },
